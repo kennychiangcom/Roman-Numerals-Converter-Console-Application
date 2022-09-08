4aa6986730467267aee51377e9fc17055a14d051
kennychiangcom <kenny@chiang.com>
2022-09-08 11:58:32 +0100

    Fixed Tests and added input checks

---
+++ Roman Numerals Converter Console Application/RomanNumeralsConverter.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace RomanNumeralsConverter
+{
+    public class RomanNumeralsConverter
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Roman numerals: ");
+            int num = RConvert(Console.ReadLine());
+            if (num == -1) Console.WriteLine("The Roman numerals you entered are invalid - please revise.");
+            else Console.WriteLine(num);
+        }
+
+        public static int RConvert(string num)
+        {
+            num = num.ToUpper().Trim();
+            if (num.Contains("IIII")) return -1;
+            else if (num.Contains("VV")) return -1;
+            else if (num.Contains("XXXX")) return -1;
+            else if (num.Contains("LL")) return -1;
+            else if (num.Contains("CCCC")) return -1;
+            else if (num.Contains("DD")) return -1;
+            else if (num.Contains("MMMM")) return -1;
+
+            int[] numValue = new int[num.Length];
+            for (int i = 0; i < num.Length; i++)
+            {
+                switch (num.Substring(i, 1))
+                {
+                    case "M":
+                        numValue[i] = 1000;
+                        break;
+                    case "D":
+                        numValue[i] = 500;
+                        break;
+                    case "C":
+                        numValue[i] = 100;
+                        break;
+                    case "L":
+                        numValue[i] = 50;
+                        break;
+                    case "X":
+                        numValue[i] = 10;
+                        break;
+                    case "V":
+                        numValue[i] = 5;
+                        break;
+                    case "I":
+                        numValue[i] = 1;
+                        break;
+                    default:
+                        return -1;
+                }
+            }
+            for (int i = 0; i < numValue.Length - 1; i++)
+            {
+                if (numValue[i] < numValue[i + 1]) numValue[i] = -numValue[i];
+            }
+            return numValue.Sum();
+        }
+    }
+}
---
+++ Roman Numerals Converter Console Application/RomanNumeralsConverterTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace RomanNumeralsConverter.Tests
+{
+    public class RomanNumeralsConverterTests
+    {
+        private RomanNumeralsConverter RomanNumeralsConverter;
+
+        [SetUp]
+        public void Setup()
+        {
+            RomanNumeralsConverter = new RomanNumeralsConverter();
+        }
+
+        [Test]
+        public void TestConvertingValidNumerals()
+        {
+            RomanNumeralsConverter.RConvert("i").Should().Be(1);
+            RomanNumeralsConverter.RConvert("III").Should().Be(3);
+            RomanNumeralsConverter.RConvert("iv").Should().Be(4);
+            RomanNumeralsConverter.RConvert("vi").Should().Be(6);
+            RomanNumeralsConverter.RConvert("viii").Should().Be(8);
+            RomanNumeralsConverter.RConvert("MCMLXXXix").Should().Be(1989);
+            RomanNumeralsConverter.RConvert("MMMDCCCLXXXVIII").Should().Be(3888);
+        }
+        [Test]
+        public void TestConvertingInvalidNumerals()
+        {
+            RomanNumeralsConverter.RConvert("iiii").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("vv").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("xXxX").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("LL").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("CCCC").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("DD").Should().Be(-1);
+            RomanNumeralsConverter.RConvert("MMMM").Should().Be(-1);
+        }
+    }
+}    
